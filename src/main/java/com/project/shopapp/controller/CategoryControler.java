4c95bbab8b5d4fe8392039b7799ccb015ce80007
Thetiger97 <tuankiet.thptcamau@gmail.com>
2023-09-23 18:17:28 +0700

    Add UserController to Controller component

--- src/main/java/com/project/shopapp/controller/CategoryControler.java
+++ src/main/java/com/project/shopapp/controller/CategoryControler.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/categories")
+@RequestMapping("${api.prefix}/categories")
 //@Validated
 public class CategoryControler {
     // Hien thi tat ca categpries
